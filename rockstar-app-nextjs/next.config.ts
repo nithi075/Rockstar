7a3819ee867808af60b4ae35d694680ab4431726
Sam Nithish <nithishraj8055@gmail.com>
2025-06-06 12:10:17 +0530

    Update next.config.ts

--- rockstar-app-nextjs/next.config.ts
+++ rockstar-app-nextjs/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rockstar-fcga.onrender.com', // **This is your backend's domain**
+        port: '', // Leave empty if no specific port
+        pathname: '/Products/**', // Or a more general '/public/**' or specific '/Products/Gurkha_Pant_Black.png' path if needed
+      },
+      // If you have other external image sources, add them here
+    ],
+  },
 };
 
 export default nextConfig;
