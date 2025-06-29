39574e5c0df7a7d9e1751605c4e5ae7a5a11813c
Sam Nithish <nithishraj8055@gmail.com>
2025-06-29 16:44:47 +0530

    Update vite.config.js

--- Admin/Admin-Dashboard/vite.config.js
+++ Admin/Admin-Dashboard/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Your backend server
+        target: 'https://admin-backend-x8of.onrender.com', // Your backend server
         changeOrigin: true,
         secure: false,
       }
