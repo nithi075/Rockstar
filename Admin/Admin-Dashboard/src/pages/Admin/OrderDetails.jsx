3b2b38630a1ccca1c083e4558c8962071051706a
Sam Nithish <nithishraj8055@gmail.com>
2025-06-30 23:18:45 +0530

    Update OrderDetails.jsx

--- Admin/Admin-Dashboard/src/pages/Admin/OrderDetails.jsx
+++ Admin/Admin-Dashboard/src/pages/Admin/OrderDetails.jsx
@@ -1,7 +1,7 @@
 // pages/Admin/OrderDetails.jsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import api from "../../axios"; 
 
 const OrderDetails = () => {
   const { orderId } = useParams();
