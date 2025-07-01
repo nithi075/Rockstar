deea460e4b92cf4d15118df7aaf04b907ef45d23
Sam Nithish <nithishraj8055@gmail.com>
2025-07-01 15:37:57 +0530

    Update OrderList.jsx

--- Admin/Admin-Dashboard/src/pages/Admin/OrderList.jsx
+++ Admin/Admin-Dashboard/src/pages/Admin/OrderList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import api from '../../axios';
 import { Link } from "react-router-dom";
 
 export default function OrderList() {
