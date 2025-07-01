99b00faff257311ef29f9b9c519d6346cccceb1c
Sam Nithish <nithishraj8055@gmail.com>
2025-07-01 10:30:49 +0530

    Update order.js

--- Rockstar-backend/routes/order.js
+++ Rockstar-backend/routes/order.js
@@ -1,21 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
 const {
-    createOrder,
-    getAllOrders,
-    getSingleOrder,
-    deleteOrder,
+  createOrder,
+  getAllOrders,
+  getSingleOrder,
 } = require("../controllers/orderController");
 
-const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
+const { isAuthenticatedUser, isAdmin } = require("../middlewares/auth");
 
-// User routes
-router.post("/", isAuthenticatedUser, createOrder);
-router.get("/:id", isAuthenticatedUser, getSingleOrder);
-
-// Admin routes
-router.get("/", isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
-router.delete("/:id", isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+router.post("/order/new", createOrder);
+router.get("/orders", isAuthenticatedUser, isAdmin("admin"), getAllOrders);
+router.get("/orders/:id", isAuthenticatedUser, isAdmin("admin"), getSingleOrder);
 
 module.exports = router;
