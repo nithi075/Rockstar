5e121399209c08c6a8623cd5e8e06f71dc9af2a5
Sam Nithish <nithishraj8055@gmail.com>
2025-07-01 14:42:22 +0530

    Update axios.jsx

--- Admin/Admin-Dashboard/src/axios.jsx
+++ Admin/Admin-Dashboard/src/axios.jsx
@@ -1,9 +1,8 @@
-// src/axios.js
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://admin-backend-x8of.onrender.com/api/v1",
-  
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
