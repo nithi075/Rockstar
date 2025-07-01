8176e23d5dd6a23605d6148fdda31b8faa1042c2
Sam Nithish <nithishraj8055@gmail.com>
2025-07-01 14:09:19 +0530

    Update orderModel.js

--- Rockstar-backend/models/orderModel.js
+++ Rockstar-backend/models/orderModel.js
@@ -36,6 +36,47 @@ const orderSchema = new mongoose.Schema({
       required: [true, "Please enter your phone number"],
     },
   },
+  shippingInfo: {
+    address: { type: String },
+    city: { type: String },
+    state: { type: String },
+    postalCode: { type: String },
+    country: { type: String },
+  },
+  paymentInfo: {
+    id: { type: String },
+    status: { type: String },
+  },
+  itemsPrice: {
+    type: Number,
+    default: 0,
+  },
+  taxPrice: {
+    type: Number,
+    default: 0,
+  },
+  shippingPrice: {
+    type: Number,
+    default: 0,
+  },
+  totalPrice: {
+    type: Number,
+    default: 0,
+  },
+  paidAt: {
+    type: Date,
+  },
+  orderStatus: {
+    type: String,
+    default: "Processing",
+  },
+  deliveredAt: {
+    type: Date,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   createdAt: {
     type: Date,
     default: Date.now,
